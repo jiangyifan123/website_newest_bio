194fb7d40b5c9d2195fb9e88fe25bd6f832e1321
Michael Wu <wuxiaohua1011@gmail.com>
2023-12-30 12:28:07 -0800

    send email done

--- website/app/api/sendgrid/route.ts
+++ website/app/api/sendgrid/route.ts
@@ -4,12 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function POST(request: NextRequest) {
-  try {
-    const data = await request.json();
-    console.log("process.env.SENDGRID_API_KEY", process.env.SENDGRID_API_KEY)
-    console.log("request.body", data);
-    // Format HTML content to include data
-    const htmlContent = `
+    try {
+        const data = await request.json();
+        // Format HTML content to include data
+        const htmlContent = `
       <div>
         <p>Firstname: ${data.firstname}</p>
         <p>Lastname: ${data.lastname}</p>
@@ -20,22 +18,32 @@ export async function POST(request: NextRequest) {
         <p>Message: ${data.message}</p>
       </div>
     `;
-    await sendgrid.send({
-      to: "nimbusnova123@gmail.com", 
-      from: "nimbusnova123@gmail.com", 
-      subject: data.subject,
-      html: htmlContent,
-    });
-  } catch (error) {
-    console.log("Unable to send mail: ", error);
+        await sendgrid.send({
+            to: "nimbusnova123@gmail.com",
+            from: "info@nimbus-nova.com",
+            subject: data.subject,
+            html: htmlContent,
+        }).then(() => {
+            console.debug("[DEBUG] Email sent successfully")
+            return new NextResponse(JSON.stringify({}), {
+                status: 200,
+            });
+        }).catch((error) => {
+            console.log("Unable to send mail: ", error);
 
-    return new NextResponse(JSON.stringify({ "status_code": 500, "status_msg": `unable to send mail due to ${error}` }), {
-      status: 500,
+            return new NextResponse(JSON.stringify({ "status_code": 500, "status_msg": `unable to send mail due to ${error}` }), {
+                status: 500,
+            });
+        });
+    } catch (error) {
+        console.log("Unable to construct mail: ", error);
+
+        return new NextResponse(JSON.stringify({ "status_code": 500, "status_msg": `unable to send mail due to ${error}` }), {
+            status: 500,
+        });
+    }
+    return new NextResponse(JSON.stringify({}), {
+        status: 200,
     });
-  }
-  console.log("Email sent successfully")
-  return new NextResponse(JSON.stringify({}), {
-    status: 200,
-  });
-} 
+}
 
