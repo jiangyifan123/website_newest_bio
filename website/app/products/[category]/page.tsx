874ae97efa4796281e2f01649baa3eb56c6f4d45
Andyfanfan <985897924@qq.com>
2024-01-15 19:29:45 -0800

    update product list

--- website/app/products/[category]/page.tsx
+++ website/app/products/[category]/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link"
 export default function Home({ params }: { params: { category: string } }) {
     return (
         <>
-            <Layout headerStyle={3} footerStyle={3} breadcrumbTitle={""} wrapperCls="home_3" headTitle="" backgroundImage={undefined} Category={"All Products"}>
+            <Layout headerStyle={3} footerStyle={3} breadcrumbTitle={"All Products"} wrapperCls="home_3" headTitle="" backgroundImage={undefined} Category={""}>
                 <section className="portfolio-section">
                     <div className="auto-container">
                         {/*Sortable Galery*/}
