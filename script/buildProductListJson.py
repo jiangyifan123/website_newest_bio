6352c647b92f099ec0c957fe0cf1eabb36fd41da
Andyfanfan <985897924@qq.com>
2024-01-16 20:24:59 -0800

    update script

--- script/buildProductListJson.py
+++ script/buildProductListJson.py
@@ -1,11 +1,22 @@
 import docx
 import json
+import os
 
 file = r'./production description.docx'
+imagePath = r'../website/public/assets/images/products/'
+iConPath = r'../website/public/assets/images/product-icon/'
 doc = docx.Document(file)
 
 productList = {}
 stk = [productList,]
+
+
+def checkPageExists(path, pid):
+    for suffix in ['jpg', 'png', 'jpeg']:
+        filePath = os.path.join(path, f'{pid}.{suffix}')
+        if os.path.exists(filePath):
+            return filePath
+    return None
 
 
 for paragraph in doc.paragraphs:
@@ -40,8 +51,15 @@ for category_name, products in productList.items():
         procceededProductList[pid]["categories"].append(category_name)
         procceededProductList[pid]['category_idx'].append(category_idx)
         procceededProductList[pid]["infos"][category_idx] = info
-        procceededProductList[pid]['image'] = "/assets/images/gallery/gallery-1.jpg"
-        procceededProductList[pid]['overlay'] = "/assets/images/icons/icon-8.png"
+        if checkPageExists(imagePath, pid) is not None:
+            procceededProductList[pid]['image'] = checkPageExists(imagePath, pid)
+        else:
+            procceededProductList[pid]['image'] = "/assets/images/gallery/gallery-1.jpg"
+
+        if checkPageExists(iConPath, pid) is not None:
+            procceededProductList[pid]['overlay'] = checkPageExists(iConPath, pid)
+        else:
+            procceededProductList[pid]['overlay'] = "/assets/images/icons/icon-8.png"
 
 # with open('product_detail.json', 'w') as f:
 #     f.write(json.dumps(productList, indent=4))
