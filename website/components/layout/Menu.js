a79bddf3f9d8a747049b4c1b5bb9b0cab4f875e5
Andyfanfan <985897924@qq.com>
2024-01-19 04:20:57 -0800

    new photo

--- website/components/layout/Menu.js
+++ website/components/layout/Menu.js
@@ -1,10 +1,6 @@
 import Link from "next/link"
-// import { useRouter } from "next/router"
-import {getUniqueCategories} from "@/components/data/products"
 
 export default function Menu() {
-    // const router = useRouter()
-    const categories = getUniqueCategories()
     return (
         <>
             <ul className="navigation clearfix">
@@ -12,11 +8,21 @@ export default function Menu() {
                 <li><Link href="/about">About</Link></li>
                 <li className="dropdown"><Link href="/products">Products</Link>
                     <ul>
-                        {
-                            categories.map(
-                                (categoryMap, index) => <li key={index+1}><Link href={`/products/${categoryMap.idx}`}>{categoryMap.name}</Link></li>
-                            )
-                        }
+                        <li className="dropdown"><Link href="#">Amino Acids</Link>
+                            <ul>
+                                <li><Link href="/products/food_grade_amino_acids">Food Grade</Link></li>
+                                <li><Link href="/products/feed_grade_amino_acids">Feed grade</Link></li>
+                            </ul>
+                        </li>
+                        <li><Link href="/products/antioxidants">Antioxidants</Link></li>
+                        <li><Link href="/products/chemicals">Chemicals</Link></li>
+                        <li><Link href="/products/dietary_fibers">Dietary Fibers</Link></li>
+                        <li><Link href="/products/flavor_enhancers">Flavor Enhancers</Link></li>
+                        <li><Link href="/products/gums">Gums</Link></li>
+                        <li><Link href="/products/prebiotics">Prebiotics</Link></li>
+                        <li><Link href="/products/preservatives">Preservatives</Link></li>
+                        <li><Link href="/products/sweeteners">Sweeteners</Link></li>
+                        <li><Link href="/products/others">Others</Link></li>
                     </ul>
                 </li>
                 {/* <li><Link href="/services">Services</Link></li> */}
