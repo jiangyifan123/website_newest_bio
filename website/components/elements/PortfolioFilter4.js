6d1793bf0d5c64de1d87cacba8cd9ea30d0e1ce7
Andyfanfan <985897924@qq.com>
2024-02-21 13:04:20 +0800

    push update for three page

--- website/components/elements/PortfolioFilter4.js
+++ website/components/elements/PortfolioFilter4.js
@@ -1,34 +1,8 @@
 'use client'
-import { useEffect, useRef, useState, useCallback } from 'react'
+import { useCallback, useState } from "react"
 
 export default function PortfolioFilter4({ title_list, setTitle }) {
-    const isotope = useRef()
     const [filterKey, setFilterKey] = useState("")
-    useEffect(() => {
-        setTimeout(() => {
-            isotope.current = new Isotope(".items-container", {
-                itemSelector: ".masonry-item",
-                // layoutMode: "fitRows",
-                percentPosition: true,
-                masonry: {
-                    columnWidth: ".masonry-item",
-                },
-                animationOptions: {
-                    duration: 750,
-                    easing: "linear",
-                    queue: false,
-                },
-            })
-            if (category) {
-                setFilterKey(category)
-            }
-        }, 200)
-    }, [])
-    useEffect(() => {
-        if (isotope.current) {
-            isotope.current.arrange({ filter: `.${filterKey}` })
-        }
-    }, [filterKey])
     const handleFilterKeyChange = useCallback((key) => () => {
         setFilterKey(key)
         setTitle(key)
