40cb190c89326b440453ad299106f4f6d9f71a8c
Andyfanfan <985897924@qq.com>
2024-02-21 12:56:35 +0800

    push update for three page

--- website/app/products/[category]/[pid]/page.tsx
+++ website/app/products/[category]/[pid]/page.tsx
@@ -1,15 +1,22 @@
 
+'use client'
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
 import { getProductByPid } from '@/components/data/products'
 import { notFound } from 'next/navigation';
 import dynamic from 'next/dynamic'
+import { useState, useEffect } from 'react'
 const PortfolioFilter3 = dynamic(() => import('@/components/elements/PortfolioFilter3'), {
+    ssr: false,
+})
+const PortfolioFilter4 = dynamic(() => import('@/components/elements/PortfolioFilter4'), {
     ssr: false,
 })
 
 export default function Home({ params }: { params: { category: string, pid: string } }) {
     const product = getProductByPid(params.pid);
+    const [currentPro, setPro] = useState('')
+
 
     if (product === undefined) {
         return notFound()
@@ -21,11 +28,20 @@ export default function Home({ params }: { params: { category: string, pid: stri
         return notFound()
     }
 
+    useEffect(() => {
+        const product_detail = params.category === 'all' ? Object.values(product.infos)[0] : product.infos[params.category];
+        if (product_detail?.multi_desc && Object.keys(product_detail.multi_desc).length > 0) {
+            setPro(Object.keys(product_detail.multi_desc)[0])
+        }
+    }, [])
 
     return (
         <>
             <Layout headerStyle={3} footerStyle={3} headTitle={product.title} breadcrumbTitle={product.title} Category={params.category} wrapperCls="home_3" backgroundImage={'/assets/newestBiotech/images/banner/agar_banner.jpeg'}>
                 <section className="project-details">
+                    {
+                        currentPro !== '' && <PortfolioFilter4 title_list={Object.keys(product_detail.multi_desc)} setTitle={setPro}/>
+                    }
                     <div className="auto-container">
                         <div className="top-box">
                             <div className="row">
@@ -46,7 +62,7 @@ export default function Home({ params }: { params: { category: string, pid: stri
                                             <div className="project-info-wrapper">
                                                 {
                                                     Object.entries(product_detail).map(([infoTitle, infoTextList]) => {
-                                                        if (['desc', 'pdfUrl', 'url', 'images'].indexOf(infoTitle) > -1) {
+                                                        if (['desc', 'pdfUrl', 'url', 'images', 'multi_desc'].indexOf(infoTitle) > -1) {
                                                             return <></>
                                                         }
                                                         return <div className="project-info">
@@ -69,7 +85,19 @@ export default function Home({ params }: { params: { category: string, pid: stri
                                     <h4>Description</h4>
                                     <div className="text">
                                         {
+                                            currentPro === '' &&
                                             product_detail.desc.map(content => {
+                                                if (content.trim().length > 0) {
+                                                    return <>
+                                                        <p>{content}</p>
+                                                        <br></br>
+                                                    </>
+                                                }
+                                            })
+                                        }
+                                        {
+                                            currentPro !== '' && 
+                                            product_detail.multi_desc[currentPro].desc.map(content => {
                                                 if (content.trim().length > 0) {
                                                     return <>
                                                         <p>{content}</p>
@@ -84,7 +112,7 @@ export default function Home({ params }: { params: { category: string, pid: stri
                                     </div>
                                 </div>
                             </div>
-                            {product_detail.images && <PortfolioFilter3 images_list={product_detail.images}/>}
+                            {product_detail?.multi_desc?.[currentPro]?.images && <PortfolioFilter3 images_list={product_detail?.multi_desc?.[currentPro]?.images}/>}
                         </div>
                     </div>
                 </section>
